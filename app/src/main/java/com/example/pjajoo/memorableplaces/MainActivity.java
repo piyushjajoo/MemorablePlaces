1bee8eaba540e615642e8659c2efc2cd9ef39b50
Piyush Jajoo <piyush.jajoo1991@gmail.com>
2018-02-06 20:01:10 -0600

    Memorable places app without permanent storage

---
+++ app/src/main/java/com/example/pjajoo/memorableplaces/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.pjajoo.memorableplaces;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final List<String> places = new ArrayList<>();
+    static final List<LatLng> locations = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView listView = (ListView) findViewById(R.id.myList);
+
+        places.add("Add a new place...");
+        locations.add(new LatLng(0, 0));
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber", position);
+                startActivity(intent);
+            }
+        });
+    }
+}
